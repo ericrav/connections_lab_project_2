9a52db01d982c4f83bf021122d477d8f33a62274
Eric Rabinowitz <ericrabinowitz@me.com>
2023-11-05 16:04:30 -0500

    https option

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => {
-  const domain = `http://localhost:${port}`;
+const server = process.env.USE_HTTPS
+  ? (() => {
+      const key = fs.readFileSync('./key.pem');
+      const cert = fs.readFileSync('./cert.pem');
+      https.createServer({ key: key, cert: cert }, app);
+    })()
+  : app;
+
+server.listen(port, () => {
+  const domain = `${process.env.USE_HTTPS ? 'https' : 'http'}://localhost:${port}`;
   console.log(`Server is listening at ${domain}`);
 });
