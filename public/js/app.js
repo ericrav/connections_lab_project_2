70bd0d5b6a7d2931495a90f4de47111c52a80736
Eric Rabinowitz <ericrabinowitz@me.com>
2023-11-05 16:41:23 -0500

    set up face tracking

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,19 @@
-window.addEventListener('load', () => {
-  console.log('hello')
+window.addEventListener('load', async () => {
+  const videoEl = await setupWebcam();
 
+  await initFaceTracking(videoEl);
 });
+
+async function setupWebcam() {
+  const videoEl = document.getElementById('webcam');
+
+  const stream = await navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .catch((err) => {
+      console.error(err);
+      alert('To play, please enable camera access in your browser settings');
+    });
+  videoEl.srcObject = stream;
+
+  return videoEl;
+}
---
+++ public/js/face-tracking.js
@@ -0,0 +1,43 @@
+const faceapi = window.faceapi;
+const model = faceapi.nets.tinyFaceDetector;
+
+async function initFaceTracking(videoEl) {
+  await faceapi.loadFaceLandmarkModel('/models');
+  await model.load('/models');
+
+  const canvas = document.getElementById('canvas');
+
+  const loop = async () => {
+    await detectFace(videoEl, canvas);
+    requestAnimationFrame(loop);
+  };
+  requestAnimationFrame(loop);
+}
+
+function getFaceDetectorOptions() {
+  const inputSize = 512;
+  const scoreThreshold = 0.5;
+  return new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold });
+}
+
+async function detectFace(videoEl, canvas) {
+  if (videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded()) {
+    return;
+  }
+
+  const options = getFaceDetectorOptions();
+
+  const result = await faceapi
+    .detectSingleFace(videoEl, options)
+    .withFaceLandmarks();
+
+  if (result) {
+    const dims = faceapi.matchDimensions(canvas, videoEl, true);
+    const resizedResult = faceapi.resizeResults(result, dims);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedResult);
+  }
+}
+
+function isFaceDetectionModelLoaded() {
+  return !!model.params;
+}
