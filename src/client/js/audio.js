19bfe55de62d3afc35bb8adef92b3fa140261278
Eric Rabinowitz <ericrabinowitz@me.com>
2023-11-05 20:17:53 -0500

    tone wip

--- src/client/js/audio.js
+++ src/client/js/audio.js
@@ -1,7 +1,30 @@
 import * as Tone from 'tone';
 
 export function playNote() {
-  const synth = new Tone.Synth().toDestination();
+  Tone.start();
+  const vol = new Tone.Volume(-18).toDestination();
+  const synth = new Tone.Synth().connect(vol);
 
-  synth.triggerAttackRelease("C4", "8n");
+  let freq = 20;
+  let volume = -18;
+  synth.triggerAttack(freq);
+  synth.volume.value = volume;
+
+  window.addEventListener('mousemove', (e) => {
+    const x = e.clientX / window.innerWidth;
+    const y = e.clientY / window.innerHeight;
+
+    freq = x * 1000;
+    volume = y * -25;
+  });
+
+  setInterval(() => {
+    synth.setNote(freq);
+    synth.volume.value = volume;
+    if (volume <= -18) {
+      vol.mute = true;
+    } else {
+      vol.mute = false;
+    }
+  }, 0);
 }
