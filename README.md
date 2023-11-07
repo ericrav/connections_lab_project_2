50fc7d103f5804fc5a674e9b65c9c32da8785e4b
Eric Rabinowitz <ericrabinowitz@me.com>
2023-11-07 12:50:25 -0500

    https local server

--- README.md
+++ README.md
@@ -7,6 +7,23 @@ npm run dev
 
 Open project at http://localhost:3000
 
+### Run with HTTPS locally
+
+First create `cert.pem` & `key.pem` following CLI prompts
+
+```
+openssl req -x509 -newkey rsa:2048 -keyout keytmp.pem -out cert.pem -days 365
+openssl rsa -in keytmp.pem -out key.pem
+```
+
+Run server:
+
+```
+USE_HTTPS=1 npm run dev
+```
+
+Open project at https://localhost:3000 (& ignore browser's security warning)
+
 ## Libraries Used
 
 - [face-api](https://github.com/justadudewhohacks/face-api.js)
--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,26 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
-const ViteExpress = require("vite-express");
+const ViteExpress = require('vite-express');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-ViteExpress.listen(app, port, () => {
-  const domain = `http://localhost:${port}`;
+const { USE_HTTPS } = process.env;
+
+const server = USE_HTTPS
+  ? (() => {
+      const key = fs.readFileSync('./key.pem');
+      const cert = fs.readFileSync('./cert.pem');
+      return https.createServer({ key: key, cert: cert }, app);
+    })()
+  : app;
+
+server.listen(port, () => {
+  const domain = `${USE_HTTPS ? 'https' : 'http'}://localhost:${port}`;
   console.log(`Server is listening at ${domain}`);
 });
+
+ViteExpress.bind(app, server);
